e814137ed430843784d3fb5a07b5e15cc781cc21
Tom Gray <tom.c.gray@gmail.com>
2018-10-27 15:09:29 +0100

    grammar tests

--- src/Utils/Grammar.js
+++ src/Utils/Grammar.js
@@ -1,33 +1,48 @@
 const Random = require("random-js")();
 
-export default {
-    getRandomVerbForDamageType: damageType => {
-        switch (damageType) {
-            case 'bludgeoning':
-            case 'unarmed':
-                return Random.pick([
-                    'bludgeon',
-                    'bash',
-                    'smash',
-                    'beat',
-                    'whack',
-                    'smack',
-                ]);
-        }
-    },
-    listItems: (items = []) => {
-        if (items.length <= 0) {
-            return '';
-        }
-        switch (items.length) {
-            case 1:
-                return items[0].name
-            case 2:
-                return items.map(i => i.name).join(' and ')
-            default:
-                const lastItem = items.pop();
-                // three or more should list them all except the last one, then "and" the last one
-                return `${items.map(i => i.name).join(', ')} and ${lastItem.name}`;
-        }
+const getRandomVerbForDamageType = damageType => {
+    switch (damageType) {
+        case 'bludgeoning':
+        case 'unarmed':
+            return Random.pick([
+                'bludgeon',
+                'bash',
+                'smash',
+                'beat',
+                'whack',
+                'smack',
+            ]);
     }
+}
+
+const listItems = (items = []) => {
+    if (items.length <= 0) {
+        return '';
+    }
+    switch (items.length) {
+        case 1:
+            return items[0].name
+        case 2:
+            return items.map(i => i.name).join(' and ')
+        default:
+            const lastItem = items.pop();
+            // three or more should list them all except the last one, then "and" the last one
+            return `${items.map(i => i.name).join(', ')} and ${lastItem.name}`;
+    }
+}
+
+const formSecondPersonIsHoldingSentence = items => {
+    return `is holding your ${listItems(items)}`
+}
+
+const formSecondPersonIsWearingSentence = items => {
+    return `is wearing your ${listItems(items)}`
+}
+
+
+export default {
+    getRandomVerbForDamageType,
+    listItems,
+    formSecondPersonIsHoldingSentence,
+    formSecondPersonIsWearingSentence
 }
--- src/Utils/Grammar.test.js
+++ src/Utils/Grammar.test.js
@@ -2,8 +2,8 @@ const assert = require('assert');
 import Grammar from './Grammar';
 const { Weapon } = require('../Item/Item')
 
-describe('Grammar', () => {
-  test('Grammar - list items - 1 item', () => {
+describe('Grammar - listItems', () => {
+  test('1 item', () => {
     const items = []
     items.push(Weapon({
         name: 'broomstick',
@@ -12,7 +12,7 @@ describe('Grammar', () => {
     expect(Grammar.listItems(items)).toBe('broomstick');
   });
 
-  test('Grammar - list items - 2 items', () => {
+  test('2 items', () => {
     const items = []
     items.push(Weapon({
         name: 'sword',
@@ -25,7 +25,7 @@ describe('Grammar', () => {
     expect(Grammar.listItems(items)).toBe('sword and axe');
   });
 
-  test('Grammar - list items - 3 items', () => {
+  test('3 items', () => {
     const items = []
     items.push(Weapon({
         name: 'sword',
@@ -42,7 +42,7 @@ describe('Grammar', () => {
     expect(Grammar.listItems(items)).toBe('sword, axe and hammer');
   });
 
-  test('Grammar - list items - 3 items, different order', () => {
+  test('3 with different order', () => {
     const items = []
     items.push(Weapon({
         name: 'hammer',
@@ -57,5 +57,47 @@ describe('Grammar', () => {
     }))
   
     expect(Grammar.listItems(items)).toBe('hammer, sword and axe');
+  });
+});
+
+describe('Grammar - formIsHoldingSentence', () => {
+  test('1 item', () => {
+    const items = []
+    items.push(Weapon({
+        name: 'bow',
+    }))
+  
+    expect(Grammar.formSecondPersonIsHoldingSentence(items)).toBe('is holding your bow');
+  });
+
+  test('4 items', () => {
+    const items = []
+    items.push(Weapon({ name: 'arrow' }))
+    items.push(Weapon({ name: 'glove' }))
+    items.push(Weapon({ name: 'bowtie' }))
+    items.push(Weapon({ name: 'axe' }))
+  
+    expect(Grammar.formSecondPersonIsHoldingSentence(items)).toBe('is holding your arrow, glove, bowtie and axe');
+  });
+});
+
+describe('Grammar - formIsWearingSentence', () => {
+  test('1 item', () => {
+    const items = []
+    items.push(Weapon({
+        name: 'bow',
+    }))
+  
+    expect(Grammar.formSecondPersonIsWearingSentence(items)).toBe('is wearing your bow');
+  });
+
+  test('4 items', () => {
+    const items = []
+    items.push(Weapon({ name: 'arrow' }))
+    items.push(Weapon({ name: 'glove' }))
+    items.push(Weapon({ name: 'bowtie' }))
+    items.push(Weapon({ name: 'axe' }))
+  
+    expect(Grammar.formSecondPersonIsWearingSentence(items)).toBe('is wearing your arrow, glove, bowtie and axe');
   });
 });
